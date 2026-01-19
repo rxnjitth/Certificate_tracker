0cd01ca60a11b90f3dca58cb7444190d93c2c422
rxnjitth <mranjith2506@gmail.com>
2026-01-19 18:21:42 +0530

    feat: Add client-side utility functions for string manipulation, email validation, and dynamic API endpoint, and update the PostgreSQL image to version 17 in Docker Compose.

--- Client/src/Utils/utils.js
+++ Client/src/Utils/utils.js
@@ -1,20 +1,22 @@
-export const getInitials = function(input) {
-    if (!input) return '';
-    return input
-      .split(' ')          
-      .map(word => word[0]) 
-      .join('')             
-      .toUpperCase();      
-  }
-  
+export const getInitials = function (input) {
+  if (!input) return '';
+  return input
+    .split(' ')
+    .map(word => word[0])
+    .join('')
+    .toUpperCase();
+}
 
-export const emailVerification = function(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  }
+
+export const emailVerification = function (email) {
+  var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
 
 // export const API_CALL = function() {
 //   return "http://localhost:5000";
 // }
 
-export const API_CALL = "http://172.16.32.86:5000";
+export const API_CALL = window.location.hostname === "localhost" 
+  ? "http://localhost:5000" 
+  : "http://172.16.32.86:5000";
